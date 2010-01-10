b43b704d965dd1c5f53c0b836f4e5dc05f99c347
ccagle8 <admin@cagintranet.com>
2010-01-10 18:18:03 +0000

    Added as per issue 18. It is empty now, but wanted it in the repository for future use
    git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@31 c6e34258-837a-11de-9f42-234e1f44043b

---
+++ admin/inc/common.php
@@ -0,0 +1,12 @@
+<?php 
+/****************************************************
+*
+* @File: 		common.php
+* @Package:	GetSimple
+* @Action:	defined variables for every page. 
+* !! Do not include any files or use relative paths in this page 	
+*
+*****************************************************/
+
+
+
