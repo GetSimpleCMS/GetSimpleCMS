c0b2dbfb3880f3b42b5e1cc17f8abfc2588388eb
ccagle8 <admin@cagintranet.com>
2010-02-02 04:09:09 +0000

    Version 2.0 Commit Point
    git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@76 c6e34258-837a-11de-9f42-234e1f44043b

--- admin/inc/configuration.php
+++ admin/inc/configuration.php
@@ -8,7 +8,7 @@
 *****************************************************/
 
 	$site_full_name = 'GetSimple';
-	$site_version_no = '2.0_beta3';
+	$site_version_no = '2.0';
 	$name_url_clean = strtolower(str_replace(' ','-',$site_full_name));
 	$site_link_back_url = 'http://get-simple.info/';
 	$ver_no_clean = str_replace('.','',$site_version_no);
