e2035f1cce306d2eb5144fd94dacd7e376550d50
ccagle8 <admin@cagintranet.com>
2010-04-22 02:12:20 +0000

    fixed bug where the wrong side menu was highlighted when a page delete was done
    git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@154 c6e34258-837a-11de-9f42-234e1f44043b

--- admin/template/sidebar-pages.php
+++ admin/template/sidebar-pages.php
@@ -1,7 +1,7 @@
 <ul class="snav">
 	<li><a href="pages.php" accesskey="p" <?php check_menu('pages');  ?>><?php echo $i18n['SIDE_VIEW_PAGES']; ?></a></li>
 	<li><a href="edit.php" accesskey="c" <?php if((@$_GET['id'] == "") && (get_filename_id()==='edit'))  { echo 'class="current"'; } ?>><?php echo $i18n['SIDE_CREATE_NEW']; ?></a></li>
-	<?php if(@$_GET['id'] != '') { ?><li><a href="#" class="current"><?php echo $i18n['EDITPAGE_TITLE']; ?></a></li><?php } ?>
+	<?php if((@$_GET['id'] != '') && (get_filename_id()==='edit')) { ?><li><a href="#" class="current"><?php echo $i18n['EDITPAGE_TITLE']; ?></a></li><?php } ?>
 	<?php exec_action("pages-sidebar"); ?>
 </ul>
 
