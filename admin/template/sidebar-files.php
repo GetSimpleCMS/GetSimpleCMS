9b242dcf5fea65ad81977902915a46c6d29db58d
ccagle8 <admin@cagintranet.com>
2010-08-10 00:58:47 +0000

    Fix for those that don't use Uploadify - http://get-simple.info/forum/viewtopic.php?pid=5253#p5253
    git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@182 c6e34258-837a-11de-9f42-234e1f44043b

--- admin/template/sidebar-files.php
+++ admin/template/sidebar-files.php
@@ -6,7 +6,7 @@
 	
 	<?php if (defined('GSNOUPLOADIFY')) { $ftpid=null; } else { $ftpid='id="mainftp"'; } ?>
 	<li class="upload">	<form <?php echo $ftpid; ?> class="fullform" action="<?php echo htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES); ?>" method="post" enctype="multipart/form-data">
-		<p><input type="file" class="text" name="file" id="file" /></p>
+		<p><input type="file" name="file" id="file" /></p>
 		<input type="hidden" name="hash" id="hash" value="<?php echo $SESSIONHASH; ?>" />
 		<p><input type="submit" class="submit" name="submit" value="<?php echo $i18n['UPLOAD']; ?>" /></p>
 	</form></li>
