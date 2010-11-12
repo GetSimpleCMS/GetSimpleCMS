220c6601e2e05e0204b644e2d97cdb9a9738fa32
ccagle8 <admin@cagintranet.com>
2010-11-12 18:51:02 +0000

    fixes issue 82
    git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@267 c6e34258-837a-11de-9f42-234e1f44043b

---
+++ admin/load-ajax.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Load Plugin Function via AJAX
+ *
+ * Loads the plugin function pass to it
+ *
+ * @package GetSimple
+ * @subpackage Plugins
+ */
+
+# setup inclusions
+$load['plugin'] = true;
+include('inc/common.php');
+login_cookie_check();
+
+# call the requested function
+$plugin_id = $_GET['id'];
+global $plugin_info;
+
+if (function_exists($_GET['func'])) {
+	call_user_func_array($_GET['func'],array());
+} else {
+	return false;
+}
+?>
