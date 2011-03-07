363e76aef8de1bd706636bee604d1404528c255d
ccagle8 <admin@cagintranet.com>
2011-03-07 13:48:55 +0000

    git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@337 c6e34258-837a-11de-9f42-234e1f44043b

---
+++ admin/template/js/ckeditor-html/config.js
@@ -0,0 +1,11 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	config.removePlugins = 'elementspath,resize' ; // hide element path and resizer
+	config.toolbarCanCollapse = false; // hide toolbar collapse button
+};
