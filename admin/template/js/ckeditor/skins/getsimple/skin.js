1265ff3da517f261968f6907e8662bcb52791900
ccagle8 <admin@cagintranet.com>
2011-03-07 14:26:02 +0000

    git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@341 c6e34258-837a-11de-9f42-234e1f44043b

---
+++ admin/template/js/ckeditor/skins/getsimple/skin.js
@@ -0,0 +1,45 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.skins.add( 'getsimple', (function()
+{
+	return {
+		editor		: { css : [ 'editor.css' ] },
+		dialog		: { css : [ 'dialog.css' ] },
+		templates	: { css : [ 'templates.css' ] },
+		margins		: [ 0, 0, 0, 0 ],
+		init : function( editor )
+		{
+			if ( editor.config.width && !isNaN( editor.config.width ) )
+				editor.config.width -= 12;
+		}
+	};
+})() );
+
+(function()
+{
+	CKEDITOR.dialog ? dialogSetup() : CKEDITOR.on( 'dialogPluginReady', dialogSetup );
+
+	function dialogSetup()
+	{
+		CKEDITOR.dialog.on( 'resize', function( evt )
+			{
+				var data = evt.data,
+					width = data.width,
+					height = data.height,
+					dialog = data.dialog,
+					contents = dialog.parts.contents;
+
+				if ( data.skin != 'getsimple' )
+					return;
+
+				contents.setStyles(
+					{
+						width : width + 'px',
+						height : height + 'px'
+					});
+			});
+	}
+})();
