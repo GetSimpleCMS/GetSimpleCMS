dcb39f7be6f8905269eecd51ed45afb70c25c2ec
ccagle8 <admin@cagintranet.com>
2011-03-26 14:20:14 +0000

    the start of the 3.1 beta
    git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@422 c6e34258-837a-11de-9f42-234e1f44043b

--- admin/inc/configuration.php
+++ admin/inc/configuration.php
@@ -7,7 +7,7 @@
  */
 
 $site_full_name = 'GetSimple';
-$site_version_no = '3.0';
+$site_version_no = '3.1B';
 $name_url_clean = lowercase(str_replace(' ','-',$site_full_name));
 $site_link_back_url = 'http://get-simple.info/';
 $ver_no_clean = str_replace('.','',$site_version_no);
