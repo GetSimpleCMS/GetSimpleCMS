3cbcf56542c0332dc72cd46f446765ceb57e95be
ccagle8 <admin@cagintranet.com>
2012-02-19 20:16:57 +0000

    should have been committed in r660
    git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@661 c6e34258-837a-11de-9f42-234e1f44043b

--- admin/template/js/ckeditor/config.js
+++ admin/template/js/ckeditor/config.js
@@ -11,3 +11,34 @@ CKEDITOR.editorConfig = function( config )
 	config.dialog_backgroundCoverColor = '#000000';
 };
 
+
+CKEDITOR.on( 'dialogDefinition', function( ev )	{
+		var dialogName = ev.data.name;
+		var dialogDefinition = ev.data.definition;
+		ev.data.definition.resizable = CKEDITOR.DIALOG_RESIZE_NONE;
+
+		if ( dialogName == 'link' ) {
+			var infoTab = dialogDefinition.getContents( 'info' );
+			//dialogDefinition.removeContents( 'target' );
+			var advTab = dialogDefinition.getContents( 'advanced' );
+			advTab.remove( 'advLangDir' );
+			advTab.remove( 'advLangCode' );
+			advTab.remove( 'advContentType' );
+			advTab.remove( 'advTitle' );
+			advTab.remove( 'advCharset' );
+		}
+
+		if ( dialogName == 'image' ) {
+			var infoTab = dialogDefinition.getContents( 'info' );
+			infoTab.remove( 'txtBorder' );
+			infoTab.remove( 'txtHSpace' );
+			infoTab.remove( 'txtVSpace' );
+			infoTab.remove( 'btnResetSize' );
+			dialogDefinition.removeContents( 'Link' );
+			var advTab = dialogDefinition.getContents( 'advanced' );
+			advTab.remove( 'cmbLangDir' );
+			advTab.remove( 'txtLangCode' );
+			advTab.remove( 'txtGenLongDescr' );
+			advTab.remove( 'txtGenTitle' );
+		}
+});
