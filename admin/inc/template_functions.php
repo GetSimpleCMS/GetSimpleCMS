GetSimpleCMS/GetSimpleCMS

commit a1e9b2920c51471464054d3c2465785c4bed9262

added directory traversal protection on all delete file operations for delete.php
Fixed some delete functions to return the actual success status

git-svn-id: https://get-simple-cms.googlecode.com/svn/trunk@760 c6e34258-837a-11de-9f42-234e1f44043b

admin/inc/template_functions.php
@@ -62,7 +62,7 @@ function get_filename_id() {
 /**
  * Delete Pages File
  *
- * Generates HTML code to place on the body tag of a page
+ * Deletes pages data file afer making backup
  *
  * @since 1.0
  * @uses GSBACKUPSPATH
@@ -71,10 +71,19 @@ function get_filename_id() {
  * @param string $id File ID to delete
  */
 function delete_file($id) {
-	$bakfile = GSBACKUPSPATH."pages/". $id .".bak.xml";
-	$file = GSDATAPAGESPATH . $id .".xml";
-	copy($file, $bakfile);
-	unlink($file);
+
+	$bakfilepath = GSBACKUPSPATH . 'pages' . DIRECTORY_SEPARATOR;
+	$bakfile = $bakfilepath . $id .'.bak.xml';
+
+	$filepath = GSDATAPAGESPATH;
+	$file = $filepath . $id .'xml';
+
+	if(filepath_is_safe($file,$filepath)){
+		$success = copy($file, $bakfile);
+		$successdel  = unlink($file);
+		if($success && $successbak) return 'success';
+	}
+	return 'error';
 }
 
 /**
@@ -102,8 +111,14 @@ function check_perms($path) {
  * @return string
  */
 function delete_zip($id) { 
-	unlink(GSBACKUPSPATH."zip/". $id);
-	return 'success';
+	$filepath = GSBACKUPSPATH . 'zip' . DIRECTORY_SEPARATOR;
+	$file = $filepath . $id;
+
+	if(filepath_is_safe($file,$filepath)){
+		$success =  unlink($file);
+		if($success) return 'success';
+	}
+	return 'error';
 } 
 
 /**
@@ -118,14 +133,21 @@ function delete_zip($id) {
  * @return string
  */
 function delete_upload($id, $path = "") { 
-	unlink(GSDATAUPLOADPATH . $path . $id);
-	if (file_exists(GSTHUMBNAILPATH.$path."thumbnail.". $id)) {
-		unlink(GSTHUMBNAILPATH.$path."thumbnail.". $id);
-	}
-	if (file_exists(GSTHUMBNAILPATH.$path."thumbsm.". $id)) {
-		unlink(GSTHUMBNAILPATH.$path."thumbsm.". $id);
-	}
-	return 'success';
+	$filepath = GSDATAUPLOADPATH . $path;
+	$file =  $filepath . $id;
+
+	if(path_is_safe($filepath,GSDATAUPLOADPATH) && filepath_is_safe($file,$filepath)){
+		$status = unlink(GSDATAUPLOADPATH . $path . $id);
+		if (file_exists(GSTHUMBNAILPATH.$path."thumbnail.". $id)) {
+			unlink(GSTHUMBNAILPATH.$path."thumbnail.". $id);
+		}
+		if (file_exists(GSTHUMBNAILPATH.$path."thumbsm.". $id)) {
+			unlink(GSTHUMBNAILPATH.$path."thumbsm.". $id);
+		}
+		if($status) return 'success';
+	}	
+
+	return 'error';
 } 
 
 /**

admin/deletefile.php
@@ -33,10 +33,10 @@ if (isset($_GET['id'])) {
 		redirect('pages.php?upd=edit-err&type='.urlencode(i18n_r('HOMEPAGE_DELETE_ERROR')));
 	} else {	
 		updateSlugs($id);
-		delete_file($id);
+		$status = delete_file($id);
 		generate_sitemap();
 		exec_action('page-delete');
-		redirect("pages.php?upd=edit-success&id=". $id ."&type=delete");
+		redirect("pages.php?upd=edit-".$status."&id=". $id ."&type=delete");
 	}
 } 
 
@@ -52,9 +52,9 @@ if (isset($_GET['zip'])) {
 if (isset($_GET['file'])) {
 	$path = (isset($_GET['path'])) ? $_GET['path'] : "";
 	$file = $_GET['file'];
-	delete_upload($file, $path);
+	$status = delete_upload($file, $path);
 	
-	redirect("upload.php?upd=del-success&id=". $file . "&path=" . $path);
+	redirect("upload.php?upd=del-".$status."&id=". $file . "&path=" . $path);
 } 
 
 

admin/inc/security_functions.php
@@ -146,4 +146,35 @@ function validate_safe_file($file, $name, $mime){
 	}
 }
 
+/**
+ * Checks that a filepath is safe to use by checking canonicalized absolute pathname.
+ *
+ * @since 3.1.3
+ *
+ * @param string $path Unknown Path to file to check for safety
+ * @param string $pathmatch Known Path to parent folder to check against
+ * @return bool Returns true if files path resolves to your known path
+ */
+function filepath_is_safe($path,$pathmatch){
+	$realpath = realpath($path);
+	$realpathmatch = realpath($pathmatch);
+	return dirname($realpath) == $realpathmatch;
+}
+
+/**
+ * Checks that a path is safe to use by checking canonicalized absolute path
+ *
+ * @since 3.1.3
+ *
+ * @param string $path Unknown Path to check for safety
+ * @param string $pathmatch Known Path to check against
+ * @return bool Returns true if $path is a subfolder of $pathmatch
+ */
+function path_is_safe($path,$pathmatch){
+	$realpath = realpath($path);
+	$realpathmatch = realpath($pathmatch);
+
+	return strpos($realpath,$realpathmatch) === 0;
+}
+
 ?>

commit d3c29a33bce7870745f386c833a9909466f66f3c

Fix for deleting pages, broken by a1e9b29

admin/inc/template_functions.php
@@ -76,12 +76,12 @@ function delete_file($id) {
 	$bakfile = $bakfilepath . $id .'.bak.xml';
 
 	$filepath = GSDATAPAGESPATH;
-	$file = $filepath . $id .'xml';
+	$file = $filepath . $id .'.xml';
 
 	if(filepath_is_safe($file,$filepath)){
-		$success = copy($file, $bakfile);
-		$successdel  = unlink($file);
-		if($success && $successbak) return 'success';
+		$successbak = copy($file, $bakfile);
+		$successdel = unlink($file);
+		if($successdel && $successbak) return 'success';
 	}
 	return 'error';
 }