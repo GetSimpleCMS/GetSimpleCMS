90f6d5208644b1a9fe358f4d33903686031909b2
tablatronix <spam@rumental.com>
2012-12-12 16:34:42 -0600

    Added GSNOSITEMAP #422
    GSNOSITEMAP prevents sitemap creation and menu items.

--- admin/template/sidebar-theme.php
+++ admin/template/sidebar-theme.php
@@ -9,7 +9,7 @@
 	<li id="sb_theme" ><a href="theme.php"  <?php check_menu('theme');  ?> accesskey="<?php echo find_accesskey(i18n_r('SIDE_CHOOSE_THEME'));?>" ><?php i18n('SIDE_CHOOSE_THEME'); ?></a></li>
 	<li id="sb_themeedit" ><a href="theme-edit.php"  <?php check_menu('theme-edit'); ?> accesskey="<?php echo find_accesskey(i18n_r('SIDE_EDIT_THEME'));?>" ><?php i18n('SIDE_EDIT_THEME'); ?></a></li>
 	<li id="sb_components" ><a href="components.php"  <?php check_menu('components'); ?> accesskey="<?php echo find_accesskey(i18n_r('SIDE_COMPONENTS'));?>" ><?php i18n('SIDE_COMPONENTS'); ?></a></li>
-	<li id="sb_sitemap" ><a href="sitemap.php" <?php check_menu('sitemap'); ?> accesskey="<?php echo find_accesskey(i18n_r('SIDE_VIEW_SITEMAP'));?>" ><?php i18n('SIDE_VIEW_SITEMAP'); ?></a></li>
+	<?php if(!getDef('GSNOSITEMAP')){ ?> <li id="sb_sitemap" ><a href="sitemap.php" <?php check_menu('sitemap'); ?> accesskey="<?php echo find_accesskey(i18n_r('SIDE_VIEW_SITEMAP'));?>" ><?php i18n('SIDE_VIEW_SITEMAP'); ?></a></li> <?php }?>
 	<?php exec_action("theme-sidebar"); ?>
 </ul>
 
