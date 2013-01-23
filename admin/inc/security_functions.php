366cb6727a30859a144572964b5b4ac7f2ca7c60
Shawn A <spam@rumental.com>
2013-01-23 14:51:43 -0600

    Added a filter function

--- admin/inc/security_functions.php
+++ admin/inc/security_functions.php
@@ -217,3 +217,30 @@ function server_is_apache() {
 function get_Server_Software() {
     return $_SERVER['SERVER_SOFTWARE'];
 }
+
+/**
+ * Performs filtering on variable, falls back to htmlentities
+ *
+ * @since 3.3.0
+ * @param  string $var    var to filter
+ * @param  string $filter filter type
+ * @return string         return filtered string
+ */
+function var_out($var,$filter = "special"){
+	if(function_exists( "filter_var") ){
+		$aryFilter = array(
+			"string"  => FILTER_SANITIZE_STRING,
+			"int"     => FILTER_SANITIZE_INT,
+			"float"   => FILTER_SANITIZE_FLOAT,
+			"string"  => FILTER_SANITIZE_STRING,
+			"url"     => FILTER_SANITIZE_URL,
+			"email"   => FILTER_SANITIZE_EMAIL,
+			"special" => FILTER_SANITIZE_SPECIAL_CHARS,
+		);
+		if(isset($aryFilter[$filter])) return filter_var( $var, $aryFilter[$filter]);
+		return filter_var( $var, FILTER_SANITIZE_SPECIAL_CHARS);
+	}
+	else {
+		return htmlentities($var);
+	}
+}
