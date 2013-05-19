41b1feacbb6f85436f5e3570419c620271820b58
tablatronix <spam@rumental.com>
2013-05-19 08:29:40 -0500

    checkin ckeditor component plugin assets, not implemented

---
+++ admin/template/js/ckeditor/plugins/gs_components/gs_components_json.php
@@ -0,0 +1,28 @@
+<?php
+# Setup inclusions
+$load['plugin'] = true;
+
+include('../../../../../inc/common.php');
+include('../../../../../inc/theme_functions.php');
+
+
+// function get_components() {
+    global $components;
+    if (!$components) {
+         if (file_exists(GSDATAOTHERPATH.'components.xml')) {
+            $data = getXML(GSDATAOTHERPATH.'components.xml');
+            $components = $data->item;
+        } else {
+            $components = array();
+        }
+    }
+    if (count($components) > 0) {
+        foreach ($components as $component) {
+        	$components_data[] = array('(% '.(string)$component->slug.' %)',(string)$component->slug,(string)$component->slug);
+        }
+    }
+// }
+
+// convert to JSON and return 
+$jsonData = str_replace('\\"',"", json_encode($components_data));
+echo $jsonData;
