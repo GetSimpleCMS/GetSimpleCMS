8ee890a4c73aba8e2b2d8a596dae2a91c4cd18e4
Shawn A <spam@rumental.com>
2013-06-03 13:41:13 -0500

    fixes #584
    this will prevent the terrible html formatting in ck3

--- admin/template/js/ckeditor/config.js
+++ admin/template/js/ckeditor/config.js
@@ -20,6 +20,20 @@ CKEDITOR.editorConfig = function( config )
 
 };
 
+CKEDITOR.on( 'instanceReady', function( ev ) {
+	var blockTags = ['div','h1','h2','h3','h4','h5','h6','p','pre','li','blockquote','ul','ol','table','thead','tbody','tfoot','td','th',];
+	var rules = {
+		indent : true,
+		breakBeforeOpen : true,
+		breakAfterOpen : false,
+		breakBeforeClose : false,
+		breakAfterClose : true
+	};
+
+	for (var i=0; i<blockTags.length; i++) {
+		ev.editor.dataProcessor.writer.setRules( blockTags[i], rules );
+	}
+}); 
 
 CKEDITOR.on( 'dialogDefinition', function( ev )	{
 		var dialogName = ev.data.name;
