b0be8a68e2e6cc5e5ed4b05447ad3650c381c098
Shawn A <spam@rumental.com>
2013-08-16 16:01:25 -0500

    codemirror
    editorFromTextarea js function

--- admin/template/js/codemirror.getsimple.js
+++ admin/template/js/codemirror.getsimple.js
@@ -3,80 +3,78 @@ var editor;
 var loadjscssfile;
 jQuery(document).ready(function () {
 	
-		// do not know what this does, looks like old ctrl+q fold debouncer
-		// function keyEvent(cm, e) {
-		// 	if (e.keyCode == 81 && e.ctrlKey) {
-		// 		if (e.type == "keydown") {
-		// 			e.stop();
-		// 			setTimeout(function() {foldFunc(cm, cm.getCursor().line);}, 50);
-		// 		}
-		// 		return true;
-		// 	}
-		// }
+	// do not know what this does, looks like old ctrl+q fold debouncer
+	// function keyEvent(cm, e) {
+	// 	if (e.keyCode == 81 && e.ctrlKey) {
+	// 		if (e.type == "keydown") {
+	// 			e.stop();
+	// 			setTimeout(function() {foldFunc(cm, cm.getCursor().line);}, 50);
+	// 		}
+	// 		return true;
+	// 	}
+	// }
+	
+	var customTheme = editor_theme; // '<?php if(isset($theme)) echo $theme; ?>'; 
+
+	var defTheme = '';		
+	// var customTheme = themes[Math.floor(Math.random()*themes.length)];
+
+	if(customTheme && customTheme != undefined && customTheme != 'default'){
+		defTheme = customTheme;
+		var parts = defTheme.split(' ');
+		loadjscssfile("template/js/codemirror/theme/"+parts[0]+".css", "css")
+	}	
+
+	var mode = 'php';
+
+	$.fn.editorFromTextarea = function(textarea){	
+		var editor = CodeMirror.fromTextArea(textarea, {
+			lineNumbers: true,
+			matchBrackets: true,
+			indentUnit: 4,
+			indentWithTabs: true,
+			enterMode: "keep",
+			mode: mode,
+			tabMode: "shift",
+			theme: defTheme,
+			fixedGutter : true,
+			styleActiveLine : true,
+			highlightSelectionMatches: true, // {showToken: /\w/}, // for word boundaries				
+			extraKeys: {
+				"Ctrl-Q" : function(cm) { foldFunc(cm, cm.getCursor().line); },
+				"F11"    : function(cm) { setFullScreen(cm, !isFullScreen(cm)); },
+				"Esc"    : function(cm) { if (isFullScreen(cm)) setFullScreen(cm, false); },
+				"Ctrl-S" : function(cm) { customSave(cm);	}
+			},
+			saveFunction:  function(cm) { customSave(cm); },
+	        viewportMargin: Infinity //for autosizing
+		});
 		
-		var customTheme = editor_theme; // '<?php if(isset($theme)) echo $theme; ?>'; 
+		// add reference to this editor to the textarea
+		$(textarea).data('editor', editor);
 
-		var defTheme = '';		
-		// var customTheme = themes[Math.floor(Math.random()*themes.length)];
+		editor.on('change', function(cm){
+				cm.hasChange = true;
+		});
 
-		if(customTheme && customTheme != undefined && customTheme != 'default'){
-			defTheme = customTheme;
-			var parts = defTheme.split(' ');
-			loadjscssfile("template/js/codemirror/theme/"+parts[0]+".css", "css")
-		}	
+		var foldFunc = CodeMirror.newFoldFunction(CodeMirror.braceRangeFinder,'...');
+		editor.on("gutterClick", foldFunc);
 
-		var mode = 'php';
+		// add in resizing
+		$(editor.getWrapperElement()).resizable({
+			// helper: "outline", // less intensive resizing
+			resize: function() {
+			editor.setSize(null, $(this).height());
+			editor.refresh();
+			}
+		});
 
-		$(".code_edit").each(function(i,textarea) {		
-			var editor = CodeMirror.fromTextArea(textarea, {
-				lineNumbers: true,
-				matchBrackets: true,
-				indentUnit: 4,
-				indentWithTabs: true,
-				enterMode: "keep",
-				mode: mode,
-				tabMode: "shift",
-				theme: defTheme,
-				fixedGutter : true,
-				styleActiveLine : true,
-				highlightSelectionMatches: true, // {showToken: /\w/}, // for word boundaries				
-				extraKeys: {
-					"Ctrl-Q" : function(cm) { foldFunc(cm, cm.getCursor().line); },
-					"F11"    : function(cm) { setFullScreen(cm, !isFullScreen(cm)); },
-					"Esc"    : function(cm) { if (isFullScreen(cm)) setFullScreen(cm, false); },
-					"Ctrl-S" : function(cm) { customSave(cm);	}
-				},
-				saveFunction:  function(cm) { customSave(cm); },
-		        viewportMargin: Infinity //for autosizing
-			});
-			
-			// add reference to this editor to the textarea
-			$(textarea).data('editor', editor);
+		fullscreen_button(editor);			
 
-			editor.on('change', function(cm){
-					cm.hasChange = true;
-			});
+		return editor;
+	}
 
-			// var hlLine = editor.addLineClass(0, "background", "activeline");
-
-			var foldFunc = CodeMirror.newFoldFunction(CodeMirror.braceRangeFinder,'...');
-			editor.on("gutterClick", foldFunc);
-
-			editor.on("cursorActivity", function() {
-			  // highlight matching
-			  // editor.matchHighlight("CodeMirror-matchhighlight");
-			});
-
-			// add in resizing
-			$(editor.getWrapperElement()).resizable({
-			  resize: function() {
-			    editor.setSize(null, $(this).height());
-			    editor.refresh();
-			  }
-			});
-
-			fullscreen_button(editor);			
-	});
+	$(".code_edit").each(function(i,textarea) {	jQuery().editorFromTextarea(textarea); });	
 
 	function editorScrollVisible(cm){
 		var wrap = cm.getWrapperElement();		
@@ -131,6 +129,8 @@ jQuery(document).ready(function () {
 
 		var button = cmwrapper.find(".overlay_but_fullscrn a");
 		// Debugger.log(button);
+		
+		// if no button create it and add to editor
 		if(button.length == 0){
 			buttonhtml = $('<div class="overlay_but_fullscrn"></div>');
 			button = $('<a href="#"><i class="icon-fullscreen"></i></a>').appendTo(buttonhtml);
@@ -145,9 +145,7 @@ jQuery(document).ready(function () {
 		}
 
 		button.toggleClass("scrolled",scrolled);
-		button.toggleClass("hidden",cm.getScrollInfo().height < 25);
-		Debugger.log(cmwrapper);
-	
+		button.toggleClass("hidden",cm.getScrollInfo().height < 25);	
 	}
 
 	setThemeSelected(editor_theme);
