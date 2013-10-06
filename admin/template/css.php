1ffd414e054402349d9825e7df032e578a3e4699
tablatronix <spam@rumental.com>
2013-10-06 08:43:12 -0500

    another style fix

--- admin/template/css.php
+++ admin/template/css.php
@@ -266,8 +266,8 @@ html {
 .nav li .info {
 	font-family: serif;
 	border: 1px solid #73CBFF;
-	background: #0BA7FD;
-	background: -moz-linear-gradient(top, #FFFF66 0%, #FFCC33 100%);
+	background: #6FCCFF;
+	background: -moz-linear-gradient(top, #BDF2FF 0%, #6FCCFF 100%);
 	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#BDF2FF), color-stop(100%,#6FCCFF));
 }
 
