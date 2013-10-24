1521112decddde7c6f9bf8395e949b416f47e100
tablatronix <spam@rumental.com>
2013-10-24 18:38:41 -0500

    fixes silly redirecting error

--- admin/template/js/jquery.getsimple.js
+++ admin/template/js/jquery.getsimple.js
@@ -547,8 +547,10 @@ jQuery(document).ready(function () {
  	// catch all redirects for session timeout on HTTP 401 unauthorized
 	$( document ).ajaxError(function( event, xhr, settings ) {
 		// notifyInfo("ajaxComplete: " + xhr.status);
-		notifyInfo("Redirecting...");
-		if(xhr.status == 401) window.location.reload();
+		if(xhr.status == 401){
+			notifyInfo("Redirecting...");
+			window.location.reload();
+		}
 	});
 	
 	//end of javascript for getsimple
