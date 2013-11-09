c6cc0efcc2116f1e22d976b1934415b4e2f918ff
tablatronix <spam@rumental.com>
2013-11-09 13:50:38 -0600

    fixing up stylewide margins

--- admin/template/css-wide.php
+++ admin/template/css-wide.php
@@ -8,12 +8,17 @@
 
 .wrapper{
 	/* main wrapper */
-	width:99%;
+	width:100%;
 }
 
 .wrapper .nav{
 	/* top header nav wrapper */
-	width:99%;
+	width:100%;
+}
+
+.wrapper h1 {
+	/* breathing space for tabs */
+	margin-left: 15px;
 }
 
 .wrapper .nav li:first-child {
@@ -23,30 +28,49 @@
 
 .wrapper table {
 	/* tables in wrapper, eg page management lists */
-	width:99%;
+	width:100%;
+}
+
+.wrapper #pill {
+	right: 15px;
+}
+
+.wrapper .nav li.rightnav {
+	margin-right: 15px;
+	margin-left: -15px;
+}
+
+.updated {
+	margin: 0 15px 20px 15px;
+}
+
+.bodycontent {
+	margin:15px;
 }
 
 #maincontent{
 	/* wrapper for admin inputs */
 	width:100%;
 	float:left;
-
 }
 
 #maincontent .main {
 	margin-right: 256px;
 	min-width: 260px;
-	margin-left: 15px;
 }
 
 #sidebar {
 	margin-left: -225px;
 }
 
+#footer .footer-left {
+	margin: 0 15px;
+}
+
 textarea, form input.title{
 	/* resize backend textareas */
 	/* outline backend administrative input fields for emphasis */
-	width:99% ;
+	width:100% ;
 }
 
 form.manyinputs textarea{
