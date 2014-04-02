209bed8f084e625bf78222b26454392de43ba48f
Shawn A <spam@rumental.com>
2014-04-02 15:38:04 -0500

    fixes #797
    filter settings inputs, very basic
    added js cookie leakage protection

--- admin/template/js/jquery.getsimple.js
+++ admin/template/js/jquery.getsimple.js
@@ -587,4 +587,14 @@ jQuery(document).ready(function () {
 	//end of javascript for getsimple
 
 });
- 
+
+// prevent js access to cookies
+if(!document.__defineGetter__) {
+    Object.defineProperty(document, 'cookie', {
+        get: function(){return ''},
+        set: function(){return true},
+    });
+} else {
+    document.__defineGetter__("cookie", function() { return '';} );
+    document.__defineSetter__("cookie", function() {} );
+}
