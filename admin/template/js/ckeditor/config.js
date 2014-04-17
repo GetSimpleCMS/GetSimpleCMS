7a282cc1e3d7f45c3b86e26f2a76736397a1fe29
Shawn A <spam@rumental.com>
2014-04-17 12:57:11 -0500

    lint fix up config.js

--- admin/template/js/ckeditor/config.js
+++ admin/template/js/ckeditor/config.js
@@ -5,11 +5,11 @@
 CKEDITOR.editorConfig = function( config )
 {
 	// Define changes to default configuration here.
-	config.resize_dir                  = 'vertical' // vertical resize
-	config.toolbarCanCollapse          = false; // hide toolbar collapse button
-	config.dialog_backgroundCoverColor = '#000000'; // veil color for dialog popups
+	config.resize_dir                  = 'vertical'; // vertical resize
+	config.toolbarCanCollapse          = false;      // hide toolbar collapse button
+	config.dialog_backgroundCoverColor = '#000000';  // veil color for dialog popups
 	config.magicline_color             = '#CF3805'; 
-	config.allowedContent              = true; // disable acf
+	config.allowedContent              = true;       // disable acf
 
 	config.toolbar_advanced = 
 		[['Bold', 'Italic', 'Underline', 'NumberedList', 'BulletedList', 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock', 'Table', 'TextColor', 'BGColor', 'Link', 'Unlink', 'Image', 'RemoveFormat', 'Source'],
@@ -48,11 +48,13 @@ CKEDITOR.on( 'dialogDefinition', function( ev )	{
 		var dialogName = ev.data.name;
 		var dialogDefinition = ev.data.definition;
 		ev.data.definition.resizable = CKEDITOR.DIALOG_RESIZE_NONE;
+		var infoTab;
+		var advTab;
 
 		if ( dialogName == 'link' ) {
-			var infoTab = dialogDefinition.getContents( 'info' );
+			infoTab = dialogDefinition.getContents( 'info' );
 			//dialogDefinition.removeContents( 'target' );
-			var advTab = dialogDefinition.getContents( 'advanced' );
+			advTab = dialogDefinition.getContents( 'advanced' );
 			advTab.remove( 'advLangDir' );
 			advTab.remove( 'advLangCode' );
 			advTab.remove( 'advContentType' );
@@ -61,13 +63,13 @@ CKEDITOR.on( 'dialogDefinition', function( ev )	{
 		}
 
 		if ( dialogName == 'image' ) {
-			var infoTab = dialogDefinition.getContents( 'info' );
+			infoTab = dialogDefinition.getContents( 'info' );
 			infoTab.remove( 'txtBorder' );
 			infoTab.remove( 'txtHSpace' );
 			infoTab.remove( 'txtVSpace' );
 			infoTab.remove( 'btnResetSize' );
 			dialogDefinition.removeContents( 'Link' );
-			var advTab = dialogDefinition.getContents( 'advanced' );
+			advTab = dialogDefinition.getContents( 'advanced' );
 			advTab.remove( 'cmbLangDir' );
 			advTab.remove( 'txtLangCode' );
 			advTab.remove( 'txtGenLongDescr' );
@@ -88,7 +90,7 @@ $.getJSON("inc/ajax.php?list_pages_json=1", function (data){
  * CKEditor Add Local Page Link
  * This is used by the CKEditor to link to internal pages
  * @param editorObj	an editor instance
-**/
+ */
 CKEsetupLinks = function(editorObj){
 
 	if (typeof editorObj === "undefined") return;
@@ -184,7 +186,7 @@ CKEsetupLinks = function(editorObj){
 			}
 		};
 	},null,null,1); 
-}
+};
 
 
 // Helper function to get a CKEDITOR.dialog.contentDefinition object by its ID.
