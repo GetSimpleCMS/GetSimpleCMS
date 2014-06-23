9eef992421ac9117552ee03129e45780f05e50e8
tablatronix <spam@rumental.com>
2014-06-23 11:28:27 -0500

    3.3.3 beta

--- admin/inc/configuration.php
+++ admin/inc/configuration.php
@@ -7,14 +7,14 @@
  */
 
 $site_full_name     = 'GetSimple';
-$site_version_no    = '3.3.3';
+$site_version_no    = '3.3.3b';
 $name_url_clean     = lowercase(str_replace(' ','-',$site_full_name));
 $ver_no_clean       = str_replace('.','',$site_version_no);
 $site_link_back_url = 'http://get-simple.info/';
 
 $cookie_name        = lowercase($name_url_clean) .'_cookie_'. $ver_no_clean;
 $cookie_login       = 'index.php';
-$cookie_time        = '10800';  // in seconds, 3 hours 
+$cookie_time        = '10800';  // in seconds, 3 hours
 
 $api_url            = 'http://get-simple.info/api/start/v3.php';
 # $api_timeout        = 800; // time in ms defaults to 500
@@ -22,7 +22,7 @@ $api_url            = 'http://get-simple.info/api/start/v3.php';
 
 if (isset($_GET['redirect'])){
 	$cookie_redirect = $_GET['redirect'];
-} else {	
+} else {
 	$cookie_redirect = 'pages.php';
 }
 
