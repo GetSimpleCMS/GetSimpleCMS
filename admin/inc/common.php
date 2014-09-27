c0d821a2b314efb5e7c17b8c6dc57a2f20d3d45b
tablatronix <spam@rumental.com>
2014-09-26 23:23:05 -0500

    typo

--- admin/inc/common.php
+++ admin/inc/common.php
@@ -55,7 +55,7 @@ $GS_constants = array(
 );
 
 $GS_definitions = array(
-	'GSDEFAULTPAGE'        => 'pages.php'                     // Default backend index page
+	'GSDEFAULTPAGE'        => 'pages.php',                    // Default backend index page
 	'GSHEADERCLASS'        => '',                             // custom class to add to header eg. gradient
 	'GSHTTPPREFIX'         => '',                             // http slug prefix GSHTTPPREFIX.GSSLUGxx
 	'GSSLUGNOTFOUND'       => '404',                          // http slug for not found
