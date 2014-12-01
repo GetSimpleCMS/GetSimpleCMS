443486715a14ea8f606b9ef30a4a10f4978dda96
Shawn A <spam@rumental.com>
2014-12-01 12:14:30 -0600

    fixes #970

--- admin/inc/configuration.php
+++ admin/inc/configuration.php
@@ -12,9 +12,14 @@ $name_url_clean     = lowercase(str_replace(' ','-',$site_full_name));
 $ver_no_clean       = str_replace('.','',$site_version_no);
 $site_link_back_url = 'http://get-simple.info/';
 
-$cookie_name        = lowercase($name_url_clean) .'_cookie_'. $ver_no_clean;
-$cookie_login       = 'index.php';
-$cookie_time        = '10800';  // in seconds, 3 hours
+// cookie config
+$cookie_name        = lowercase($name_url_clean) .'_cookie_'. $ver_no_clean; // non-hashed name of cookie
+$cookie_login       = 'index.php'; // login redirect
+$cookie_time        = '10800';     // in seconds, 3 hours
+$cookie_path        = '/';         // cookie path
+$cookie_domain      = null;        // cookie domain
+$cookie_secure      = null;        // cookie secure only
+$cookie_httponly    = true;        // cookie http only
 
 $api_url            = 'http://get-simple.info/api/start/v3.php';
 # $api_timeout        = 800; // time in ms defaults to 500
