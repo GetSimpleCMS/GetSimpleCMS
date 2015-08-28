8878b9479763d36a3c6adf5b2354d6c3f809197f
Shawn A <shawn@iturd-Pro.local>
2015-08-27 23:05:35 -0500

    fix save_file return

--- admin/inc/basic.php
+++ admin/inc/basic.php
@@ -680,6 +680,7 @@ function XMLsave($xml, $file) {
 	if(getDef('GSFORMATXML',true)) $data = formatXmlString($data); // format xml if config setting says so
 	$data = exec_filter('xmlsave',$data); // @filter xmlsave executed before writing string to file
 	$success = save_file($file, $data); // LOCK_EX ?
+	return $success;
 }
 /**
  * JSON Save
