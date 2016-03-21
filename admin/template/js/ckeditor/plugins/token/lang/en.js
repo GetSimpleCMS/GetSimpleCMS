7b28c81dafe81ac179d94e78c412bbca17973370
Shawn A <spam@rumental.com>
2016-03-21 13:55:27 -0500

    trimming ckeditor
    removing test features

--- admin/template/js/ckeditor/plugins/token/lang/en.js
+++
@@ -1,6 +0,0 @@
-CKEDITOR.plugins.setLang( 'token', 'en', {
-	title: 'Token Insertion',
-	toolbar: 'Token',
-	name: 'Token to Insert',
-	pathName: 'token'
-} );
--- admin/template/js/ckeditor/plugins/token/lang/ru.js
+++
@@ -1,6 +0,0 @@
-CKEDITOR.plugins.setLang( 'token', 'ru', {
-	title: 'Вставить токен',
-	toolbar: 'Токен',
-	name: 'Токен',
-	pathName: 'token'
-} );
