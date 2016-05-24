efeceecce8977adc61ff34ebfe48793a73d32616
Shawn A <spam@rumental.com>
2016-05-24 12:00:54 -0500

    dropzone update 4.3.0 tested

--- admin/template/js/dropzone/dropzone-amd-module.js
+++ admin/template/js/dropzone/dropzone-amd-module.js
@@ -1,3 +1,15 @@
+// Uses AMD or browser globals to create a jQuery plugin.
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+      // AMD. Register as an anonymous module.
+      define(['jquery'], factory);
+  } else {
+      // Browser globals
+      factory(jQuery);
+  }
+} (function (jQuery) {
+    var module = { exports: { } }; // Fake component
+
 
 /*
  *
@@ -1765,3 +1777,6 @@
   contentLoaded(window, Dropzone._autoDiscoverFunction);
 
 }).call(this);
+
+    return module.exports;
+}));
