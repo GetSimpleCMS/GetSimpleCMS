cc3818232522e0c6eb8a319138e1474fb09e8bc8
iturd pro <shawn@iturd-Pro.local>
2016-09-17 16:43:44 -0500

    3.3.12
    critical fix

--- admin/inc/configuration.php
+++ admin/inc/configuration.php
@@ -7,7 +7,7 @@
  */
 
 $site_full_name     = 'GetSimple';
-$site_version_no    = '3.3.11';
+$site_version_no    = '3.3.12';
 $name_url_clean     = lowercase(str_replace(' ','-',$site_full_name));
 $ver_no_clean       = str_replace('.','',$site_version_no);
 $site_link_back_url = 'http://get-simple.info/';
