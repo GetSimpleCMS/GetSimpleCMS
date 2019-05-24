4c4c12d17f50e02f5f8778808daa35855a2db74c
tablatronix <alverson@Shawns-MacBook-Pro.local>
2019-05-24 10:00:26 -0500

    fixes #1266
    Disabled nouploadify by default

--- temp.gsconfig.php
+++ temp.gsconfig.php
@@ -46,7 +46,7 @@ define('GSDONOTPING', 1);
 #define('GSCANONICAL', 1);
 
 # Use Uploadify to upload files?
-#define('GSNOUPLOADIFY', 1);
+#define('GSNOUPLOADIFY', 0); // 0 to reenable uploadify ( uploadify is outdated flash based and has known xss exploits! USE AT OWN RISK )
 
 # WYSIWYG editor height (default 500)
 #define('GSEDITORHEIGHT', '400');
