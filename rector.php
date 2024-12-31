931fe296937c4899c35e3d4867df9c6ef47b5c0a
tablatronix <807787+tablatronix@users.noreply.github.com>
2024-12-31 16:35:10 -0600

    rector config

--- rector.php
+++ rector.php
@@ -6,21 +6,54 @@
 use Rector\Set\ValueObject\SetList;
 use Rector\Config\RectorConfig;
 // use Rector\Set\ValueObject\LevelSetList;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
+
+// remove 
+// AddLiteralSeparatorToNumberRector
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/admin',
         __DIR__ . '/plugins',
+        __DIR__ . '/extend',
     ]);
+
+    // Set DEAD_CODE
 
     // register a single rule
     // $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-    $rectorConfig->import(SetList::PHP_72);
-    $rectorConfig->import(SetList::PHP_73);
+    // $rectorConfig->rule(RemoveUnusedVariableAssignRector::class);
+    // $rectorConfig->import(SetList::PHP_72);
+    // $rectorConfig->import(SetList::PHP_73);
     $rectorConfig->import(SetList::PHP_74);
     $rectorConfig->import(SetList::PHP_80);
     // define sets of rules
     //    $rectorConfig->sets([
     //        LevelSetList::UP_TO_PHP_81
     //    ]);
+    
+    // ->withSkip([
+    //     Rector\Php54\Rector\Array_\LongArrayToShortArrayRector::class,
+    // ])
+    // ->withPreparedSets(deadCode: true);
+    // // A. whole set
+    // ->withPreparedSets(typeDeclarations: true)
+    // // B. or few rules
+    // ->withRules([
+    //     TypedPropertyFromAssignsRector::class
+    // ]);    
 };
+
+// use Rector\Config\RectorConfig;
+// use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
+
+// return RectorConfig::configure()
+//     // A. whole set
+//     ->withPreparedSets(typeDeclarations: true)
+//     // B. or few rules
+//     ->withRules([
+//         TypedPropertyFromAssignsRector::class
+//     ]);
+
+
+?>
--- rector.sh
+++ rector.sh
@@ -1,1 +1,3 @@
-vendor/bin/rector process --dry-run plugins
+composer show > rector.log
+vendor/bin/rector list-rules --output-format json extend > rector_rules_out.json
+vendor/bin/rector process --dry-run extend
